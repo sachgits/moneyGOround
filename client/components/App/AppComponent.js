a3d8f463c0f03754945cdf44756048f4541363e2
sachgits <sachgits@gmail.com>
2016-04-19 16:21:29 +0300

    changes in AppComponent

--- client/components/App/AppComponent.js
+++ client/components/App/AppComponent.js
@@ -9,15 +9,20 @@ export default class App extends React.Component {
       <div>
         <Navbar />
         <div className="greeting">
+<<<<<<< Updated upstream
           <h1>MERRY-GO-ROUND</h1>
           <p>Always a pleasure finding new jobs</p>
 
+=======
+          <h1>Where Money Goes Round</h1>
+          <p>Your Turn is Comming</p>
+>>>>>>> Stashed changes
           <img src={require('../../assets/yeoman.png')} />
         </div>
         <div className='content'>
           {this.props.children}
         </div>
-        <Footer username={this.props.viewer.username} website={this.props.viewer.website} />
+        <Footer username={"sachgits"} website={"Merry Go Round"} />
       </div>
     );
   }
