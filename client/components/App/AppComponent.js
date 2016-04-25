549eba3278911b65aa1d3bda399a26ccc382c1a3
sachgits <sachgits@gmail.com>
2016-04-25 10:46:51 +0300

    saving confilicts

--- client/components/App/AppComponent.js
+++ client/components/App/AppComponent.js
@@ -9,14 +9,9 @@ export default class App extends React.Component {
       <div>
         <Navbar />
         <div className="greeting">
-<<<<<<< Updated upstream
-          <h1>MERRY-GO-ROUND</h1>
-          <p>Always a pleasure finding new jobs</p>
 
-=======
           <h1>Where Money Goes Round</h1>
           <p>Your Turn is Comming</p>
->>>>>>> Stashed changes
           <img src={require('../../assets/yeoman.png')} />
         </div>
         <div className='content'>
